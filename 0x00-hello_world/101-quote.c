c12ef96f008a54ad92af66472e40264ecac17b09
fadwadwa <fawda1119@gmail.com>
2023-03-16 10:36:41 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line to the standard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
