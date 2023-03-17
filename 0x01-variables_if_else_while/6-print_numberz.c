ef516ff8d94a02443fb8a3d16748ae8c27c8ccee
fadwadwa <fawda1119@gmail.com>
2023-03-17 13:27:15 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: 0
+ */
+int main(void)
+{	int i;
+	char t[10]="0123456789";
+	for (i = 0 ; i < 10 ; i++)
+	putchar(t[i]);
+	return (0);
+}
