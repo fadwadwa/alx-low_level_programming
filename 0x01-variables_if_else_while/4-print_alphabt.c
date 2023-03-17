5b185a69404e4668c8ef4add1aea523f7066e33c
fadwadwa <fawda1119@gmail.com>
2023-03-17 13:03:50 -0700

    task3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: 0
+ */
+int main(void)
+{	int i;
+	char t[27] = "abcdfghijklmnoprstuvwxyz\n";
+
+	for (i = 0; i < 27 ; i++)
+	putchar(t[i]);
+	return (0);
+}
