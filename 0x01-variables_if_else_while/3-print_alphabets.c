da226d9185cc56518c4a478c83a8e609a20c46c8
fadwadwa <fawda1119@gmail.com>
2023-03-17 12:42:12 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: 0
+ */
+int main(void)
+{	int i;
+	char t[100] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\0";
+
+	for(i = 0; i < 52 ; i++)
+	putchar(t[i]);
+	return (0);
+}
