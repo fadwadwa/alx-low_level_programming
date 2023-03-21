84bfb6f944a62cc950758314396629251b965acc
fadwadwa <fawda1119@gmail.com>
2023-03-21 13:18:36 -0700

    5.Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,9 +7,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		return (1);
+		_putchar('+');
+	}
 	else if (n < 0)
+	{
 		return (-1);
+		_putchar('0');
+	}
 	else
+	{
 		return (0);
+		_putchar('-');
+	}
 }
