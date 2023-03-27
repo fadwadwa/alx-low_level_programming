9c0e457180c6ac0af55bc61457f2878d225b0ae3
fadwadwa <fawda1119@gmail.com>
2023-03-27 09:32:50 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,16 @@
  *
  * Return: 0
  */
-int _strlen(char *s);
+
 void print_rev(char *s)
 {
-	int i, len = _strlen(s);
+	int len = 0, i;
 
+	while (s[len] != '\0')
+		len++;
 	for (i = len - 1; i >= 0; i--)
 	{
-		putchar(*(s + i));
+		putchar(s[i]);
 	}
 	putchar('\n');
 }
