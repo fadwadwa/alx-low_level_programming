c546e4a1526e2600c7e169ee771e12cab08a3287
fadwadwa <fawda1119@gmail.com>
2023-03-27 07:46:22 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: a pointer to a char
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{	int i = 0, leng = 0;
+
+	while (*(str + i) != 0)
+	{
+		len++;
+		i++;
+	}
+	return (0);
+}
