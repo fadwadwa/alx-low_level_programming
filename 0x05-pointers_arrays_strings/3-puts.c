896623a398554f1d6bd91a578967eaa2c99da31a
fadwadwa <fawda1119@gmail.com>
2023-03-27 08:51:05 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: a pointer of char
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while (*str != 0)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
