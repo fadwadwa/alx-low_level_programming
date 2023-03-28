4a5f477a54917e5aa7cc796ce9f1edf5f33a4b13
fadwadwa <fawda1119@gmail.com>
2023-03-27 18:51:31 -0700

    10.100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,27 +8,33 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+	int i = 0, d = 0, n = 0, len, f, digit;
 
-	/*skip white space*/
-	while (s[i] == ' ')
-		i++;
-	/* handle sign*/
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-		i++;
-	/* process digits*/
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	return (sign * result);
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++
+	}
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
