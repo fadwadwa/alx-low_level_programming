b2b8e53b42ac68ef6d762a3ad2f68e94c3e2a61b
fadwadwa <fawda1119@gmail.com>
2023-03-27 19:14:08 -0700

    11.101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,34 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - the main function
+ * Return: 0;
+ */
+int main(void)
+{
+	srand(time(NULL)); /*Seed the random number generator*/
+
+	char password[PASSWORD_LENGTH + 1]; /*+1 for null terminator*/
+
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		/* Generate a random number from 0-61*/
+		int random_num = rand() % 62;
+
+		/*If the number is less than 26, use an uppercase letter*/
+		if (random_num < 26)
+			password[i] = 'A' + random_num;
+		/*If the number is less than 52, use a lowercase letter*/
+		else if (random_num < 52)
+			password[i] = 'a' + (random_num - 26);
+		/*Otherwise, use a digit*/
+		else
+			password[i] = '0' + (random_num - 52);
+	}
+	/*Add null terminator to the end of the string*/
+	password[PASSWORD_LENGTH] = '\0';
+	printf("Generated Password: %s\n", password);
+	return (0);
+}
