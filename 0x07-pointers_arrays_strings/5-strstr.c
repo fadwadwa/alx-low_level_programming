5f08862612bf49caad5ccfd198c2f1f179f28751
fadwadwa <fawda1119@gmail.com>
2023-04-03 19:59:12 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,37 +10,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int haystack_len = 0;
-	int needle_len = 0;
-	char *p1 = haystack;
-	char *p2 = needle;
+	char *h, *n;
 
-	while (*p1 != '\0')
+	while (*haystack != '\0')
 	{
-		haystack_len++;
-		p1++;
-	}
-	while (*p2 != '\0')
-	{
-		needle_len++;
-		p2++;
-	}
-	if (needle_len > haystack_len)
-		return (NULL);
-	p1 = haystack;
-	while (*p1 != '\0')
-	{
-		char *p3 = p1;
-		char *p4 = needle;
-
-		while (*p3 == *p4 && *p4 != '\0')
+		h = haystack;
+		n = needle;
+		while (*n != '\0' && *haystack == *n)
 		{
-			p3++;
-			p4++;
+			haystack++;
+			n++;
 		}
-		if (*p4 == '\0')
-			return (p1);
-		p1++;
+		if (!*n)
+			return (h);
+		haystack++;
 	}
-	return (NULL)
+	return (NULL);
 }
