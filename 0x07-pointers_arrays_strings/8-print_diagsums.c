5678f618628c948dccfae14a3e14e1f4e6f44bd5
fadwadwa <fawda1119@gmail.com>
2023-04-04 19:31:57 -0700

    fixed task7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,7 +15,7 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size * size; i += size + 1)
 		sum1 += a[i];
-	for (i = size - 1; i < size * (size - 1); i += size - 1)
+	for (i = size - 1; i < size * size - 1; i += size - 1)
 		sum2 += a[i];
 	printf("%d, %d\n", sum1, sum2);
 }
