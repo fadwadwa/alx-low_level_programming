fda223b587e206e55cd4547c26fbb69fd0401f3c
fadwadwa <fawda1119@gmail.com>
2023-04-06 19:42:26 -0700

    5-sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,6 +8,15 @@
  */
 int _sqrt_recursion(int n)
 {
+	int root;
+
 	if (n < 0)
 		return (-1);
-
+	if (n == 0 || n == 1)
+		return (n);
+	root = _sqrt_recursion(n / 2);
+	if ((root + 1) * (root + 1) <= n)
+		return (root + 1);
+	else
+		return (root);
+}
