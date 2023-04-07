a4025c81fb601b55fba5b3a9120b278608be25f1
fadwadwa <fawda1119@gmail.com>
2023-04-06 19:59:38 -0700

    fixed the code

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int __sqrt(int n, int i);
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: an integer
@@ -8,15 +10,22 @@
  */
 int _sqrt_recursion(int n)
 {
-	int root;
-
 	if (n < 0)
 		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	root = _sqrt_recursion(n / 2);
-	if ((root + 1) * (root + 1) <= n)
-		return (root + 1);
-	else
-		return (root);
+	return (__sqrt(n, 0));
+}
+
+/**
+ *
+ *
+ *
+ *
+ */
+int __sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (- 1);
+	if (i * i == n)
+		return (i);
+	return (__sqrt(n, i + 1));
 }
