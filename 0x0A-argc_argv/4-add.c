4e88dc24da85788ed06cf547f3edcb13346dad29
fadwadwa <fawda1119@gmail.com>
2023-04-11 16:58:25 -0700

    fixed code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,19 +9,21 @@
  */
 int main(int argc, char **argv)
 {
-	int i, j,sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != 0; j++)
+		char *num = argv[i];
+
+		for (j = 0; num[j] != '\0'; j++)
 		{
-			if (isdigit(argv[i][j]) == 0)
+			if (isdigit(num[j]) == 0)
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		sum += atoi(argv[i]);
+		sum += atoi(num);
 	}
 	printf("%d\n", sum);
 	return (0);
