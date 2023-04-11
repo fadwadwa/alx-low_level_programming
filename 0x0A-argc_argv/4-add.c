e30aee3da8c72574ac4dbce8a30a037b76be1286
fadwadwa <fawda1119@gmail.com>
2023-04-10 20:23:53 -0700

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,16 +9,19 @@
  */
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, j,sum = 0;
 
-	if (argc == 1)
+	if (argc < 3)
 		printf("0\n");
-	for (i = 2; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		if (isdigit(argv[i]) == 0)
+		for (j = 0; argv[i] != 0; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (isdigit(argv[i][j]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}
