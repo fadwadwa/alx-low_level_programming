3618f2a4c623e95ad0daa1ae70c0d02dfa6969cb
fadwadwa <fawda1119@gmail.com>
2023-04-13 19:01:33 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,6 +24,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	result = (char *) malloc(sizeof(char) * (len1 + n + 1));
 	if (result == NULL)
 		return (NULL);
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
 	for (i = 0; i < n; i++)
 		result[len1 + i] = s2[i];
 	result[len1 + n] = '\0';
