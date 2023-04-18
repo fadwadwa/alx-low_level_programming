aa006a6fb3750729b9fe40c87bff2822bc98c4de
fadwadwa <fawda1119@gmail.com>
2023-04-17 20:34:32 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - infos of the cute dog
+ * @name: name of the dog
+ * @age: his/her age
+ * @owner: his/her owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
