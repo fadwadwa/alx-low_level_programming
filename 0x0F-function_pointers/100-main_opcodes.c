ff92e044fd0d75f9a76365b02b1f128547ac0068
fadwadwa <fawda1119@gmail.com>
2023-04-21 09:41:49 -0700

    100-main_opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main fct
+ * @argc: arguments counter
+ * @argv: arguments vector
+ * Return: the opcodes
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	int i, num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	unsigned char *main_ptr = (unsigned char *)main;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", *(main_ptr + i));
+	}
+	printf("\n");
+	return (0);
+}
