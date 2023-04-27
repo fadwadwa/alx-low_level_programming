ed93707790f470a36c10b5dc64fc022fc0fa54b7
fadwadwa <fawda1119@gmail.com>
2023-04-27 16:09:32 -0700

    fixed the errors

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,13 @@
 #ifndef LISTS_H
-#define LIST_H
-
-int _putchar(char c);
-size_t print_list(const list_t *h);
+#define LISTS_H
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  */
 typedef struct list_s
@@ -17,5 +16,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
