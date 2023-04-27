064faf16a47de819badcfdf50a878db12da59183
fadwadwa <fawda1119@gmail.com>
2023-04-27 16:38:56 -0700

    changed some lines

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,11 +14,6 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new == NULL)/*failed allocation*/
 		return (NULL);
-	if (str_copy == NULL)/* if the dup failed*/
-	{
-		free(new);
-		return (NULL);
-	}
 	new->str = str_copy;/*assign the dup string to the new node*/
 	new->next = *head;/* make the new node the head */
 	*head = new;/*update the head*/
