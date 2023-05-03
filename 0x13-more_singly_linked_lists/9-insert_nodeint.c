db1ab5ee580d43a47d131051d8edc670b9c6d20e
fadwadwa <fawda1119@gmail.com>
2023-05-02 19:41:47 -0700

    test test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,8 +8,8 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0;
 	listint_t *new, *p;
+	unsigned int count = 1;
 
 	if (head != NULL)
 		return (NULL);
