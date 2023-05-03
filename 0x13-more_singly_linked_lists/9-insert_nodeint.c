40c3cabc285d001803a85448d2e97b3da8ce1f99
fadwadwa <fawda1119@gmail.com>
2023-05-02 19:38:51 -0700

    test test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,8 +16,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	p = *head;
 	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	p = *head;
 	while (idx != count)
 	{
 		p = p->next;
